859d975148140a256871ce4cb60e01132b0de679
Kleber Gonçalves Pereira <kleber_master@hotmail.com>
2023-04-04 18:52:03 -0300

    adicionada agenda, database não implementado

--- app/src/main/java/com/kleber/beautyplanner/Agenda/AgendaFragment.kt
+++ app/src/main/java/com/kleber/beautyplanner/Agenda/AgendaFragment.kt
@@ -35,7 +35,7 @@ class AgendaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agenda, container, false)
+        return inflater.inflate(R.layout.activity_agenda, container, false)
     }
 
     companion object {
